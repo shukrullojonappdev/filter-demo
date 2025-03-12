35287c969ad830bcd3a514df95b4ffb67b753ef3
shukrullojondevnu <shukrullojon.devnu@gmail.com>
2025-03-12 16:19:55 +0500

    commited

---
+++ src/app/advanced-filter/advanced-filter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advanced-filter',
+  imports: [],
+  templateUrl: './advanced-filter.component.html',
+  styleUrl: './advanced-filter.component.css'
+})
+export class AdvancedFilterComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { AdvancedFilterComponent } from './advanced-filter/advanced-filter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SearchBarComponent, AdvancedFilterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'filter-demo';
